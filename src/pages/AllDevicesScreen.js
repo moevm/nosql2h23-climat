c8538da8e67be7790a7a0149d6345a8779ec63d6
Ionina Ksenia <ionina2002@yandex.ru>
2023-12-25 14:31:04 +0300

    fix pages

--- src/pages/AllDevicesScreen.js
+++ src/pages/AllDevicesScreen.js
@@ -1,32 +1,38 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
+import { useState, useEffect } from 'react'
+
 import aircond_img from '../imgs/airconditioner.png'
 import purifier_img from '../imgs/purifierpng.png'
 
+import NavBar from '../components/NavBar'
+import DeviceProfile from '../components/DeviceProfile'
+
 function AllDevicesScreen() {
-  return (
-    <div>
-        <NavBar></NavBar>
-        <div className="component_grid">
-                <div className="grid_item">
-                    <img className="image_item" src={aircond_img}></img>
-                    <h3 className="item_text">Air conditioner</h3>
-                </div>
-                <div className="grid_item">
-                    <img className="image_item" src={purifier_img}></img>
-                    <h3 className="item_text">Air purifier</h3>
-                </div>
-                <div className="grid_item">
-                    <img className="image_item" src={aircond_img}></img>
-                    <h3 className="item_text">Air conditioner</h3>
-                </div>
-                <div className="grid_item">
-                    <img className="image_item" src={purifier_img}></img>
-                    <h3 className="item_text">Air purifier</h3>
-                </div>
+
+    const [devices, setDevices] = useState([]);
+    const [openModel,setOpenModal] = useState(false);
+
+    useEffect(() => {
+        fetch('/devices')
+            .then((res) => res.json())
+            .then((result)=> setDevices(result.data))
+      }, []);
+    
+    return (
+        <div>
+            {openModel && <DeviceProfile closeModal={setOpenModal}></DeviceProfile>}
+            <NavBar></NavBar>
+            <div className="component_grid">
+                {devices.map((device)=>(
+                    <div className="grid_item">
+                        <button className="roomModalBtn" onClick={()=>setOpenModal(true)}>
+                        <img className="image_item" src={aircond_img}></img>
+                        <h3 className="item_text">{device}</h3></button>
+                    </div>
+                ))}
             </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default AllDevicesScreen
--- src/pages/AllRoomScreen.js
+++ src/pages/AllRoomScreen.js
@@ -1,43 +1,43 @@
 import React from "react";
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
 import room_img from '../imgs/room1.jpg';
 import NavBar from "../components/NavBar";
 import RoomProfile from "../components/RoomProfile";
 
-function RoomsScreen({ roomArray }) {
 
+function RoomsScreen() {
+    const [rooms, setRooms] = useState([]);
     const [openModel,setOpenModal] = useState(false);
+    const [graphic_data, setGraphic] = useState();
+    
+    useEffect(() => {
+        fetch('/rooms')
+            .then((res) => res.json())
+            .then((result)=> {setRooms(result.data)})
+        fetch('/rooms/getdata')
+            .then((res) => res.json())
+            .then((result)=> {setGraphic(result.data)})
+      },
+       []);
+    // let  time_co=graphic_data.map(function (user) {
+    //    return (user.time)
+    //   });
+    // let  val_co = graphic_data.map(function (user) {
+    //     return (user.value)
+    //    });
+    //gtime_co={time_co}
     return (
         <div>
-            {openModel && <RoomProfile closeModal={setOpenModal}></RoomProfile>}
+            {openModel && <RoomProfile closeModal={setOpenModal} ></RoomProfile>} 
             <NavBar></NavBar>
             <div className="component_grid">
+                {rooms.map((room)=>(
                 <div className="grid_item">
-                    <button className="roomModalBtn" onClick={()=>setOpenModal(true)}>
+                    <button className="roomModalBtn" onClick={()=>{setOpenModal(true)}}>
                     <img className="image_item" src={room_img}></img>
-                    <h3 className="item_text">room 1</h3></button>
+                    <h3 className="item_text">{room.replace('_',' ')}</h3></button>
                 </div>
-                <div className="grid_item">
-                    <img className="image_item" src={room_img}></img>
-                    <h3 className="item_text">room 2</h3>
-                </div>
-                <div className="grid_item">
-                    <img className="image_item" src={room_img}></img>
-                    <h3 className="item_text">room 3</h3>
-                </div>
-                <div className="grid_item">
-                    <img className="image_item" src={room_img}></img>
-                    <h3 className="item_text">room 4</h3>
-                </div>
-                <div className="grid_item">
-                    <img className="image_item" src={room_img}></img>
-                    <h3 className="item_text">room 5</h3>
-                </div>
-                <div className="grid_item">
-                    <img className="image_item" src={room_img}></img>
-                    <h3 className="item_text">room 6</h3>
-                </div>
+                ))}
             </div>
         </div>
     )
