3a69d9fa7cd5403b29f6455a25991c38d0fbb673
vbezuprechnaya <vberezenok@gmail.com>
2023-12-25 14:58:03 +0300

    current state v.1 with graphics

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter,  RouterProvider} from "react-router-dom";
-import App from './App';
+import { createBrowserRouter,  RouterProvider, Route} from "react-router-dom";
 import RoomsScreen from './pages/AllRoomScreen';
+import App from './App';
 import AllDevicesScreen from './pages/AllDevicesScreen';
 import AllScenarioScreen from './pages/AllScenarioScreen';
 import NotificationScreen from './pages/NotificationScreen';
+import RoomProfile from './components/RoomProfile';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    // element: <RoomsScreen/>,
+    element: <RoomsScreen/>,
   },
   {
     path: "/rooms",
