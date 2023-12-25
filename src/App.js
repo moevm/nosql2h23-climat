1c84035762ec2e70ae6ab7cf0a1eecc654c377e2
Ionina Ksenia <ionina2002@yandex.ru>
2023-12-25 14:28:12 +0300

    fix App route

--- src/App.js
+++ src/App.js
@@ -3,34 +3,15 @@ import React from 'react'
 import { useState,useEffect } from 'react';
 import NavBar from './components/NavBar';
 import RoomsScreen from './pages/AllRoomScreen';
+import Button from '@mui/material/Button';
 
 function App() {
-  
-  const [state, setState] = useState(null);
-
-  const callBackendAPI = async () => {
-    const response = await fetch('/express_influx');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-    return body;
-  };
-  
-  // получение GET маршрута с сервера Express, который соответствует GET из server.js 
-  useEffect(() => {
-    callBackendAPI()
-    .then(res => setState(res.express))
-    .catch(err => console.log(err));
-  }, [])
-
   return (
     <div>
       <NavBar></NavBar>
       <p>test page</p>
+      <Button variant="contained">Hello world</Button>;
       <div>
-          {state}
       </div>
     </div>
   );
